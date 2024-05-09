a052d8e25b26b5e10171312c927b622ebf8fb13d
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2024-05-09 15:34:52 +0100

    Update setVis.py
    latest

---
+++ Houdini/setVis.py
@@ -0,0 +1,13 @@
+#setVis
+#sets -Use Visibility- to 0 on the alembics inside selected ObjNodes
+#icon = ROP_alembic
+
+times = 0
+parents = hou.selectedNodes()
+for parent in parents:
+    abc = parent.children()[0]
+    print(abc.path)
+    abc.setParms({"usevisibility": 0})
+    times += 1
+
+hou.ui.displayMessage("visibility set for "+str(times)+" alembics!")
