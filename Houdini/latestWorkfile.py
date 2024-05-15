ad2cd7058e9acbfaaf992a1a12fe8a7d9af6dc05
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2024-05-15 10:23:16 +0100

    Update latestWorkfile.py
    added workfile list feature

--- Houdini/latestWorkfile.py
+++ Houdini/latestWorkfile.py
@@ -1,7 +1,31 @@
-import os
-#load latest workfile for:
-workfile = '/job/comms/dyson_n683_2006645/dn030/dn030_0060/work/fx/fx/houdini/workfile/dn030_0060_fx_default_v001.hip'
+#latestWorkfile
+#loads the latest workfile from a selection. Meant to be customized manually for a show.
 
+
+
+
+import hou, os
+
+##########################################################
+#ADD YOUR WORKFILES HERE:
+
+#add it to option
+options = ("s0080")
+#...and create a variable of the same name containing the workfile path
+
+s0080 = """/job/comms/dyson_n683_2006645/dn030/dn030_0080/work/fx/fx/houdini/workfile/dn030_0080_fx_default_v001.hip"""
+
+##########################################################
+
+#choose workfile:
+choice = hou.ui.selectFromList(options,exclusive=True)
+#choice = hou.ui.displayMessage(str(node),buttons=options) button options instead of list
+choice = int(choice[0])
+
+workfile = (eval(str(options[choice])))
+
+
+#load the latest workfile:
 file = workfile.split("/")[-1]
 path = workfile.replace(file,"")
 
