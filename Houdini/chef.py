154194c46754fc859d9141e7646e6c5069978e5a
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2025-01-24 18:07:25 +0000

    Update chef.py
    now actually dives into OBJ subnetworks. its not quite good yet but actually does something now...

--- Houdini/chef.py
+++ Houdini/chef.py
@@ -4,7 +4,6 @@
 
 import hou
 
-root = hou.node("/obj/")
 frame = (int(hou.frame()),int(hou.frame()))
 selected_nodes = hou.selectedNodes()
 
@@ -12,3 +11,8 @@ selected_nodes = hou.selectedNodes()
 for node in selected_nodes:
     print(node)
     node.cook(force=True,frame_range=(frame))
+    for child in node.children():
+        child.cook(force=True,frame_range=(frame))
+        if child.type().category() == hou.objNodeTypeCategory():
+            for childchild in child.children(): 
+                childchild.cook(force=True,frame_range=(frame))
