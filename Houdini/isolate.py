3aa4df97fcab60ea3d774c6bcb2a49e9d433d066
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2025-02-10 11:48:19 +0000

    Update isolate.py
    overhaul to work restoring without selection

--- Houdini/isolate.py
+++ Houdini/isolate.py
@@ -6,19 +6,28 @@ import hou
 import toolutils
 import htoa
 
-network = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
-networkpath = network.pwd().path()
-sel = hou.selectedNodes()[0]
-mat_out = hou.node(networkpath+"/OUT_material")
 
-#try connect selected node to OUT or reconnect previously connected
-try:
-    if hou.session.maximsCurrentMatNode:
-        mat_out.setInput(0,hou.session.maximsCurrentMatNode)
-        hou.session.maximsCurrentMatNode = None
-    else:
-        hou.session.maximsCurrentMatNode = mat_out.input(0)
-        mat_out.setInput(0,sel)
-except:
-        hou.session.maximsCurrentMatNode = mat_out.input(0)
-        mat_out.setInput(0,sel)
+def isolateSelOrRestore():
+    network = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
+    networkpath = network.pwd().path()
+    mat_out = hou.node(networkpath+"/OUT_material")
+    
+    #try connect selected node to OUT or reconnect previously connected
+    try:
+        if hou.session.maximsCurrentMatNode:
+            mat_out.setInput(0,hou.session.maximsCurrentMatNode)
+            hou.session.maximsCurrentMatNode = None
+        else:
+            if not hou.selectedNodes():
+                print("nothing selected")
+                return
+            hou.session.maximsCurrentMatNode = mat_out.input(0)
+            sel = hou.selectedNodes()[0]
+            mat_out.setInput(0,sel)
+    except:
+            hou.session.maximsCurrentMatNode = mat_out.input(0)
+            sel = hou.selectedNodes()[0]
+            mat_out.setInput(0,sel)
+
+isolateSelOrRestore()
+            
