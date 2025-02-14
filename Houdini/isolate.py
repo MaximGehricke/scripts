480a92b54fd77d6712fc36def635b683ecbecea7
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2025-02-14 16:45:47 +0000

    Update isolate.py
    can now grab shopnet path from currently visible network editor (so will also work if you're on one of many open tabs)

--- Houdini/isolate.py
+++ Houdini/isolate.py
@@ -8,8 +8,9 @@ import htoa
 
 
 def isolateSelOrRestore():
-    network = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
-    networkpath = network.pwd().path()
+    networkpath = next(tab.pwd().path() for tab in hou.ui.currentPaneTabs() if isinstance(tab, hou.NetworkEditor) and tab.isCurrentTab())
+    # network = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
+    # networkpath = network.pwd().path()
     mat_out = hou.node(networkpath+"/OUT_material")
     
     #try connect selected node to OUT or reconnect previously connected
